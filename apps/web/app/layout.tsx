689d2be98d5390efc7daed9b1f92f7c4adabc4b4
tooooo1 <wer4272@nate.com>
2023-06-27 20:55:37 +0900

    feat(web): kakao map logic

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,6 +7,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <Script
+        type="text/javascript"
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY}&autoload=false`}
+      />
       <body>{children}</body>
     </html>
   );
