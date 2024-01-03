6f424b5934d4ebf898ebbc3727a779bfdf296d34
Chung-il Jung <wer4272@nate.com>
2024-01-03 18:08:08 +0900

    feat: Introducing stylex (#9)
    ### Description
    
    <!--
      ✍️ Write a short summary of your work.
      If necessary, include relevant screenshots.
    -->
    
    Introducing stylex
    
    ### Testing Instructions
    
    <!--
      Give a quick description of steps to test your changes.
    -->

--- frontend/apps/web/next.config.js
+++ frontend/apps/web/next.config.js
@@ -1,4 +1,24 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ *
+ */
+
 /** @type {import('next').NextConfig} */
-module.exports = {
-  transpilePackages: [],
-};
+const stylexPlugin = require('@stylexjs/nextjs-plugin');
+const babelrc = require('./.babelrc.js');
+const plugins = babelrc.plugins;
+const [, options] = plugins.find(
+  (plugin) => Array.isArray(plugin) && plugin[0] === '@stylexjs/babel-plugin',
+);
+const rootDir = options.unstable_moduleResolution.rootDir ?? __dirname;
+
+module.exports = stylexPlugin({
+  rootDir,
+  useCSSLayers: true,
+})({
+  transpilePackages: ['@stylexjs/open-props'],
+});
