0bbbf800f6e9cbcb5e3f7e28ce84baab72df91d2
Chung-il Jung <wer4272@nate.com>
2024-01-03 17:41:54 +0900

    feat: stylex

--- frontend/apps/web/.eslintrc.js
+++ frontend/apps/web/.eslintrc.js
@@ -1,9 +1,14 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@seoul-awake/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
+  extends: ['@seoul-awake/eslint-config/next.js'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@stylexjs'],
   parserOptions: {
     project: true,
+  },
+  rules: {
+    '@stylexjs/valid-styles': 'error',
+    'no-unused-vars': 'off',
   },
 };
