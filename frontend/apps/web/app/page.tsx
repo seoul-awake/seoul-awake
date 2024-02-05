9f77e4e7077103be1837f1bd6fc53ac95279e1e1
Chung-il Jung <jungchungil7@gmail.com>
2024-02-05 23:06:52 +0900

    feat: custom overlay

--- frontend/apps/web/app/page.tsx
+++ frontend/apps/web/app/page.tsx
@@ -1,5 +1,28 @@
 import Map from '../components/client/Map/Map';
+import MapOverlay from '../components/client/Map/MapOverlay';
+
+const locations = [
+  { lat: 37.526072, lng: 126.864301 }, // 목동
+  { lat: 37.6438741, lng: 126.6690268 }, // 장기
+];
 
 export default function Home() {
-  return <Map />;
+  return (
+    <>
+      <Map />
+      {locations.map((position) => (
+        <MapOverlay key={position.lat} position={position}>
+          <div
+            style={{
+              backgroundColor: 'black',
+              padding: '10px',
+              borderRadius: '8px',
+            }}
+          >
+            tooooo1
+          </div>
+        </MapOverlay>
+      ))}
+    </>
+  );
 }
