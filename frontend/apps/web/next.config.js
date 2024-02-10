f9bb53033371c8ba01043e7e52ca46c2ef2fa8c6
Chung-il Jung <jungchungil7@gmail.com>
2024-02-10 13:01:19 +0900

    migration: stylex version

--- frontend/apps/web/next.config.js
+++ frontend/apps/web/next.config.js
@@ -8,17 +8,17 @@
  */
 
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const stylexPlugin = require('@stylexjs/nextjs-plugin');
 const babelrc = require('./.babelrc.js');
 const plugins = babelrc.plugins;
-const [, options] = plugins.find(
+const [_name, options] = plugins.find(
   (plugin) => Array.isArray(plugin) && plugin[0] === '@stylexjs/babel-plugin',
 );
 const rootDir = options.unstable_moduleResolution.rootDir ?? __dirname;
+const aliases = options.aliases ?? undefined;
+const useCSSLayers = options.useCSSLayers ?? undefined;
 
-module.exports = stylexPlugin({
-  rootDir,
-  useCSSLayers: true,
-})({
+module.exports = stylexPlugin({ rootDir, aliases, useCSSLayers })({
   transpilePackages: ['@stylexjs/open-props'],
 });
